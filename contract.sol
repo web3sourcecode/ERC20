e5307f008927f9374d0c9a979d5e49ffea98a548
web3sourcecode <148886865+web3sourcecode@users.noreply.github.com>
2023-10-25 21:51:56 -0500

    Update contract.sol
    ERC20 token requirements

--- contract.sol
+++ contract.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: Unlicensed
 pragma solidity ^0.8.18;
 
 contract enrichedExperiences{
@@ -6,23 +7,56 @@ string cryptoCurrencyName;
 string cryptoCurrencySymbol;
 uint256 amountOfCryptoTransacted;
 uint256 totalSupplyOfEE; 
-uint256 balance;
+uint256 walletBalance;
+bool success;
+uint256 remaining;
+address spender;
+
+
+
 /*struct walletInfo{
     uint256 userEEBalance;
     address userWalletAddress;}
     */
 //address public userWalletAddress;
-//address  public _owner;
-// address public owner = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
 address public owner;
 constructor(){
 owner = msg.sender;
 }
-function balanceOf(address _owner) public view returns (uint256 balance){address owner = address(this);
 
+mapping (address => uint ) public walletInfo;
 
+function retrieveWalletInfo (address _owner , uint balance) public{
+    walletInfo[msg.sender] = balance;
+}
+function balanceOf(address _owner) public view returns (uint256 balance){
+    return balance;
 }
 
+
+function transfer(address _to, uint amount) public view returns (bool success) {
+    return success;
+}
+
+function transferFrom(address _from, address _to, uint amount ) public view returns (bool success){
+    return success;
+}
+
+function approve (address _spender, uint _amount) public view returns (bool success){
+    return success;
+}
+
+function allowance (address _owner, address _spender) public view returns (uint remaining){
+    return remaining;
+}
+
+function returnTestAddressBalance (uint256 _walletBalance) public {
+    walletBalance = _walletBalance;
+}
+
+function returnTestWalletBalance() public view returns  (uint256) {
+    return walletBalance;
+}
 function nameSetting(string memory _cryptoCurrencyName) public{
 cryptoCurrencyName = _cryptoCurrencyName;
 }
@@ -40,9 +74,11 @@ totalSupplyOfEE = _totalSupplyOfEE;
 function name() public view returns (string memory) {
 return cryptoCurrencyName;
 }
+
 function symbol() public view returns (string memory) {
 return cryptoCurrencySymbol;
 }
+
 function decimals() public view returns(uint256){
 return amountOfCryptoTransacted;
 }
@@ -51,6 +87,8 @@ function totalSupply() public view returns(uint256){
 }
 
 function amountOfCryptocurrencySize() public view returns (uint256){
-return amountOfCryptoTransacted;}
-
+return amountOfCryptoTransacted;
 }
+
+event Transfer(address indexed _from, address indexed _to, uint256 _value);
+event Approval(address indexed _owner, address indexed _spender, uint256 _value);}
